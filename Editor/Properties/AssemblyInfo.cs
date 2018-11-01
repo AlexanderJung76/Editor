3e48dd347456eb7f0f8f3b6f3f6c25bd85a309d1
Alexander Jung <alexanderj@gmx.net>
2018-11-01 21:34:04 +0100

    Assembly info added

--- Editor/Properties/AssemblyInfo.cs
+++ Editor/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("Editor")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("lil School Project - Text Editor")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyCompany("Chaos Inc.")]
 [assembly: AssemblyProduct("Editor")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
